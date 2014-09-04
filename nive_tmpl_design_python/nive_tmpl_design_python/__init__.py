939b6a529c70bc379d610fad96ec4dea0e28c333
adroullier <arndt@nive.co>
2014-09-04 16:20:50 +0200

    use default bootstrap 3

--- nive_tmpl_design_python/nive_tmpl_design_python/__init__.py
+++ nive_tmpl_design_python/nive_tmpl_design_python/__init__.py
@@ -1,7 +1,7 @@
 
 from nive.definitions import ViewModuleConf
 
-configuration = ViewModuleConf("nive_cms.design.view",
+configuration = ViewModuleConf("nive_cms_design_bs3.view",
     static="nive_tmpl_design_python:static",
     staticName="tmpldesign",
     templates="nive_tmpl_design_python:templates"
